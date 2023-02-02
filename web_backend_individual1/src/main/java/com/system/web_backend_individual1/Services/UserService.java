5b040ab30d4d3e97a1660b6ad8f1aa0eed591b27
pantherer <amulbashyal6@gmail.com>
2023-02-02 12:24:59 +0545

    update

--- web_backend_individual1/src/main/java/com/system/web_backend_individual1/Services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.system.web_backend_individual1.Services;
-
-import com.system.web_backend_individual1.Pojo.UserPojo;
-
-public interface UserService {
-    String save_user(UserPojo userPojo);
-}
