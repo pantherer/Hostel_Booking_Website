d3f1e283fe62b441823ced1b8298190891aa8a98
pantherer <amulbashyal6@gmail.com>
2023-02-02 12:23:43 +0545

    uupdate

--- web_backend_individual1/src/main/java/com/system/web_backend_individual1/Repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.system.web_backend_individual1.Repo;
-import com.system.web_backend_individual1.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
--- web_backend_individual1/src/main/java/com/system/web_backend_individual1/Services/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.system.web_backend_individual1.Services;
-import com.system.web_backend_individual1.Pojo.CustomerPojo;
-
-public interface CustomerService {
-
-    String save_customer(CustomerPojo customerPojo);
-}
